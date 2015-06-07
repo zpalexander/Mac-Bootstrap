2284c1ae5531cc096b9d2d84800a51a93160d459
Zach Alexander <zach@elephantventures.com>
2015-06-07 11:32:47 +0800

    Set up 'Done' message for main script - update Mac OS X before we do anything else

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
-# Variables
+# Set the colours you can use
+black='\033[0;30m'
+white='\033[0;37m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+# Resets the style
+reset=`tput sgr0`
+
+# Color-echo
+# arg $1 = message
+# arg $2 = Color
+cecho() {
+  echo "${2}${1}${reset}"
+  return
+}
+
+# Start by updating the OS
+sudo softwareupdate --list
+sudo softwareupdate --install --all
 
 # Install programs for the Mac
 bash scripts/install-programs.sh
@@ -17,3 +39,10 @@ bash scripts/github-ssh.sh
 
 # Set up Sublime
 bash scripts/configure-sublime.sh
+
+echo ""
+cecho "############################" $green
+cecho "#        YOU'RE DONE       #" $green
+cecho "#            YAY           #" $green
+cecho "############################" $green
+echo ""
